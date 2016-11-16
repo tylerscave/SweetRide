08f6c6521774385ac252f0ad3ba484207d68a37a
Tyler Jones <tylerjones365@gmail.com>
2016-11-15 17:33:54 -0800

    moved test code into tylersTest() method for modularity

--- src/application/LoginSystem.java
+++ src/application/LoginSystem.java
@@ -19,41 +19,52 @@ public class LoginSystem {
     protected static final String USER = "root";
     protected static final String PASS = "";
 
+    /**
+     * Main is the entry point to SweetRide
+     * @param args
+     */
     public static void main(String[] args) {
         // TODO
 
 
-
         //Testing calls for Tyler's queries
+        tylersTest();
+    }
+
+    /**
+     * tylersTest() is a test function that will be deleted when project is complete
+     * please use the code contained in this method however you need
+     */
+    private static void tylersTest() {
         UserSystem userSystem = new UserSystem();
         try {
             
             //ResultSet transResult = userSystem.searchByTransmission("MANUAL");
-            //System.out.println("Results for search by transmission");
+            //System.out.println("Results for search by transmission:");
             //userSystem.printVehicles(transResult);
             
             //ResultSet classResult = userSystem.searchByClass("LUXURY");
-            //System.out.println("Results for search by class");
+            //System.out.println("Results for search by class:");
             //userSystem.printVehicles(classResult);
             
             //ResultSet zipCodeResult = userSystem.searchByZipCode(95113);
-            //System.out.println("Results for search by zip code");
+            //System.out.println("Results for search by zip code:");
             //userSystem.printVehicles(zipCodeResult);
             
             //ResultSet cityStateResult = userSystem.searchByCityState("Los Angeles", "California");
-            //System.out.println("Results for search by city and state");
+            //System.out.println("Results for search by city and state:");
             //userSystem.printVehicles(cityStateResult);
             
             //ResultSet makeResult = userSystem.searchByMake("Toyota");
-            //System.out.println("Results for search by make");
+            //System.out.println("Results for search by make:");
             //userSystem.printVehicles(makeResult);
             
             //ResultSet makeModelResult = userSystem.searchByMakeModel("Toyota", "Supra");
-            //System.out.println("Results for search by make and model");
+            //System.out.println("Results for search by make and model:");
             //userSystem.printVehicles(makeModelResult);
             
             ResultSet yearResult = userSystem.searchByYear(2016);
-            System.out.println("Results for search by year");
+            System.out.println("Results for search by year:");
             userSystem.printVehicles(yearResult);
         } catch (SQLException e) {
             e.printStackTrace();
